ae1d9cec6368c7fdf3bd0380dcf5689e06a3575a
Salvatore Capuozzo <sasy.19.05.96@gmail.com>
2017-02-07 11:32:19 +0100

    Aggiunto oggetto LaserRangefinder

--- Phi6 Alpha/LaserRangefinder.swift
+++ Phi6 Alpha/LaserRangefinder.swift
@@ -56,6 +56,44 @@ class LaserRangefinder : SKSpriteNode, Sensor {
         return sprite
     }
     
+    func laser(scene: GameScene) {
+        if self.orientation == "Horizontal" {
+            let startL = CGPoint(x: self.position.x - self.size.width/2 - 1 + (scene.frame.size.width / 2), y: -self.position.y + (scene.frame.size.height / 2) + 10)
+            
+            let endL = CGPoint(x: self.position.x - self.size.width/2 - 1 + (scene.frame.size.width / 2) - 1000, y: -self.position.y + (scene.frame.size.height / 2) + 10)
+            
+            let pathL = UIBezierPath.arrow(from: startL, to: endL,
+                                           tailWidth: 2.0, headWidth: 5.0, headLength: 5.0)
+            
+            let startR = CGPoint(x: self.position.x + self.size.width/2 + 1 + (scene.frame.size.width / 2), y: -self.position.y + (scene.frame.size.height / 2) + 10)
+            
+            let endR = CGPoint(x: self.position.x + self.size.width/2 + 1 + (scene.frame.size.width / 2) + 1000, y: -self.position.y + (scene.frame.size.height / 2) + 10)
+            
+            let pathR = UIBezierPath.arrow(from: startR, to: endR,
+                                           tailWidth: 2.0, headWidth: 5.0, headLength: 5.0)
+            /*
+            scene.shapeLayerL.path = pathL.cgPath
+            scene.shapeLayerL.strokeColor = UIColor.red.cgColor
+            scene.shapeLayerL.lineWidth = 2.0
+            
+            scene.shapeLayerR.path = pathR.cgPath
+            scene.shapeLayerR.strokeColor = UIColor.red.cgColor
+            scene.shapeLayerR.lineWidth = 2.0
+            
+            scene.view?.layer.addSublayer(scene.shapeLayerL)
+            scene.view?.layer.addSublayer(scene.shapeLayerR)
+ */
+        }
+    }
+    
+    func setDistance(scene: GameScene) {
+        if self.orientation == "Horizontal" {
+            self.value = abs(Float(scene.phisphere.position.x) - Float(self.position.x))
+        } else if self.orientation == "Vertical" {
+            self.value = abs(Float(scene.phisphere.position.y) - Float(self.position.y))
+        }
+    }
+    
     func unset() {
         self.value = 0
     }
