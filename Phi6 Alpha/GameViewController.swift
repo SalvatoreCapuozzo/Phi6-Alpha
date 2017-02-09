8518c368c2f73a7d4232c9b512b5c5aee73b0bc5
ariemmov <ergoproxy8@gmail.com>
2017-02-09 14:33:43 +0100

    crash eirosdofidfpog

--- Phi6 Alpha/GameViewController.swift
+++ Phi6 Alpha/GameViewController.swift
@@ -388,6 +388,10 @@ class GameViewController: UIViewController, SKSceneDelegate, UIGestureRecognizer
         return title
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        audioPlayer.stop()
+    }
+    
     @IBAction func setMass() {
         scene.phisphere.physicsBody?.mass = CGFloat(massSlider.value)
         massLabel.text! = String(describing: round((scene.phisphere.physicsBody?.mass)!*10)/10)
